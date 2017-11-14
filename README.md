d2445e2c21f879c8dffaf7d17f01878ec327568f
luckylittle <lucian.maly@versent.com.au>
2017-11-14 14:23:32 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# What are the most frequently used keywords in DevOps jobs in Australia?
+
+## Usage
+
+```bash
+pip install --user BeautifulSoup sklearn numpy scipy
+./devops_jobs_keywords.py
+<- WAIT! ->
+7787
+aws 493
+experience 354
+integration 351
+continuous 326
+strong 319
+continuous integration 288
+infrastructure 227
+puppet 215
+understanding 213
+automation 211
+design 207
+technology 188
+cloud 167
+development 158
+software 157
+highly 157
+read 152
+leading 149
+systems 149
+sydney 145
+engineer 145
+background 144
+linux 141
+devops 135
+team 133
+```
+
+# Inspired by book "Principles of Data Science" by "Sinam Ozdemir"
---
+++ devops_jobs_keywords.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import requests # used to grab data from the web
+from BeautifulSoup import BeautifulSoup # used to parse HTML
+from sklearn.feature_extraction.text import CountVectorizer # used to count number of words and phrases
+
+texts = [] # initializing texts that will hold our job descriptions in this list
+
+for index in range(0,1000,10): # go through 100 pages of Australian indeed
+    page = 'http://au.indeed.com/jobs?q=devops&start='+str(index) # identify the url of the job listings
+    web_result = requests.get(page).text # use requests to actually visit the url
+    soup = BeautifulSoup(web_result) # parse the html of the resulting page
+    for listing in soup.findAll('span', {'class':'summary'}): # for each listing on the page
+        texts.append(listing.text) # append the text of the listing to our list
+
+type(texts) # == list
+vect = CountVectorizer(ngram_range=(1,2), stop_words='english') # get basic counts of one and two word phrases
+matrix = vect.fit_transform(texts) # fit and learn to the vocabulary in the corpus
+print len(vect.get_feature_names()) # how many features are there
+
+freqs = [(word, matrix.getcol(idx).sum()) for word, idx in vect.vocabulary_.items()] #sort from largest to smallest
+for phrase, times in sorted (freqs, key = lambda x: -x[1])[:25]:
+    print phrase, times # print most used keywords and their frequency
